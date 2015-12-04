4f2d3e17234164412b85e39c9c39614254a6bd48
krishnateja614 <krishnateja@pec.edu>
2015-12-04 15:14:07 -0700

    remove inst directory

--- inst/examples/shiny/server.R
+++
@@ -1,8 +0,0 @@
-library(LDAvis)
-library(shiny)
-shinyServer(function(input, output, session) {
-  output$myChart <- renderVis({
-    with(TwentyNewsgroups,
-         createJSON(phi, theta, doc.length, vocab, term.frequency,
-                    R = input$nTerms))})
-})
--- inst/examples/shiny/ui.R
+++
@@ -1,7 +0,0 @@
-library(LDAvis)
-shinyUI(
-  fluidPage(
-    sliderInput("nTerms", "Number of terms to display", min = 20, max = 40, value = 30),
-    visOutput('myChart')
-  )
-)
